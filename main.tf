6aaab211117f2ef772121c5c8aceecef523caad5
an0n <anoobis@systemli.org>
2022-02-10 16:10:00 +0100

    Added VMware vdc provider and updated Terraform syntax

--- main.tf
+++ main.tf
@@ -1,14 +1,23 @@
 #===============================================================================================================
 # vCloud Director Provider definition, the latest version as of today is 2.4.0
 #===============================================================================================================
+
+terraform {
+  required_providers {
+    vcd = {
+      source = "vmware/vcd"
+      version = "3.5.1"
+    }
+  }
+}
+
 provider "vcd" {
-  version					= "2.3.0" // Provider version
-  user                 		= "${var.vcd_user}"
-  password             		= "${var.vcd_pass}"
-  org                  		= "${var.vcd_org}"
-  vdc                      = "${var.vcd_vdc}"
-  url                  		= "${var.vcd_url}"
+  user                 		= var.vcd_user
+  password             		= var.vcd_pass
+  org                  		= var.vcd_org
+  vdc                     = var.vcd_vdc
+  url                  		= var.vcd_url
 
   max_retry_timeout    		= "45" // Retry time for api
-  allow_unverified_ssl 		= "true"
+  allow_unverified_ssl 		= "false"
 }
